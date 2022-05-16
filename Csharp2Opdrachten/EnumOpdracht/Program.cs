1a72f5a45ead220dbd5e605051af017f22b62155
Mavis de Ridder <89397118+pinkflamey@users.noreply.github.com>
2022-05-16 10:46:55 +0200

    finished enum opdracht

---
+++ Csharp2Opdrachten/EnumOpdracht/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EnumOpdracht
+{
+    internal enum Colors
+    {
+        Red,
+        Green,
+        Blue,
+        Pink
+    }
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Colors[] colors = (Colors[])Enum.GetValues(typeof(Colors));
+
+            foreach (Colors color in colors)
+            {
+                Console.WriteLine(color.ToString());
+            }
+            
+            string value = Console.ReadLine();
+
+            Colors parsedColor;
+
+            try
+            {
+                parsedColor = (Colors)Enum.Parse(typeof(Colors), value);
+                Console.WriteLine("You have chosen the color: " + parsedColor.ToString());
+            }
+            catch
+            {
+                Console.WriteLine("This color is not available.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
