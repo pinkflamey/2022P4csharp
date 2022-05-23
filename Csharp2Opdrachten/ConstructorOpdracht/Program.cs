bc545a27459710032642c47008751beff0196965
Mavis de Ridder <89397118+pinkflamey@users.noreply.github.com>
2022-05-23 09:55:07 +0200

    base

---
+++ Csharp2Opdrachten/ConstructorOpdracht/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConstructorOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
