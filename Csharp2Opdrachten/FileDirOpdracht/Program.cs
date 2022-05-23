cb2ab68a3bb9f235294aa2f2037ed191de0cfdbe
Mavis de Ridder <89397118+pinkflamey@users.noreply.github.com>
2022-05-23 12:23:31 +0200

    finished filediropdrach

---
+++ Csharp2Opdrachten/FileDirOpdracht/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace FileDirOpdracht
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo dirinfo = new DirectoryInfo("C:/Users/pinkf/Documents/Mediacollege Amsterdam/Bewijzenmap/Periode 1.4/GD/test");
+
+            Console.WriteLine("Listing all files, subdirectories and their files in C:/Users/pinkf/Documents/Mediacollege Amsterdam/Bewijzenmap/Periode 1.4/GD/test \n");
+
+            ProcessDirectory(dirinfo);
+
+            Console.ReadLine();
+        }
+
+        public static void ProcessDirectory(DirectoryInfo targetDirectory)
+        {
+            Console.WriteLine("\n" + targetDirectory.Name.ToString());
+
+            FileInfo[] fileEntries = targetDirectory.GetFiles();
+            foreach (FileInfo file in fileEntries)
+                ProcessFile(file);
+
+            DirectoryInfo[] subdirectoryEntries = targetDirectory.GetDirectories();
+            foreach (DirectoryInfo subdirectory in subdirectoryEntries)
+                ProcessDirectory(subdirectory);
+        }
+
+        public static void ProcessFile(FileInfo file)
+        {
+            Console.WriteLine("--File: " + file.Name.ToString());
+        }
+
+    }
+}
