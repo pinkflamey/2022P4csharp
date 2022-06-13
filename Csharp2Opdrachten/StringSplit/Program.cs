e3ed661b8c0624c42dc604b3b2b3e593d503284e
Mavis de Ridder <89397118+pinkflamey@users.noreply.github.com>
2022-06-13 10:51:59 +0200

    update and stringsplit

---
+++ Csharp2Opdrachten/StringSplit/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace StringSplit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText(@"C:\Users\pinkf\Documents\Mediacollege Amsterdam\Bewijzenmap\Periode 1.4\GD\2022P4csharp\Csharp2Opdrachten\StringSplit\stringsplit.txt");
+
+            string[] data = text.Split(":");
+
+            string[] data2 = data[1].Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine(data[0] + "\n");
+            for(int i = 0; i < data.Length; i++)
+            {
+                data2[i] = data2[i].Replace(" ", "");
+            }
+            foreach (string s in data2)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadLine();
+        
+        }
+    }
+}
